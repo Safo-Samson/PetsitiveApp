2924fa1abf134f4981b511c4c7c6c40c46efb678
Samson Safo <95703627+Safo-Samson@users.noreply.github.com>
2024-12-17 09:07:35 +0000

    added filter and sign in templates

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { FilterComponent } from './filter/filter.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: SignInComponent },
+  { path: 'filter', component: FilterComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
